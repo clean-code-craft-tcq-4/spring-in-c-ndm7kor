64bb32ed97851c3cef3d3f042df5d66338845d3f
ndm7kor <105267513+ndm7kor@users.noreply.github.com>
2022-05-19 15:06:45 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -9,15 +9,7 @@ struct Stats {
   float min;
 };
 
-#define NUMBER_OF_ALERTS 2
-
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
-
-typedef void (*alerter_funcptr)();
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
-void emailAlerter(void);
-void ledAlerter(void);
-
 
 #endif
